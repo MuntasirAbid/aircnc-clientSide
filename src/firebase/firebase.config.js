67c7feae17cc5cd3664db99fd50f792e9f578d83
MuntasirAbid <muntasiralabid@gmail.com>
2023-01-24 21:55:20 +0100

    created login and signUp page, authontation implimentation done with email, password and google

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBo62byUBem-mCLo-cKGiz5XTT3e21oJnQ",
+  authDomain: "air-cnc-5f07c.firebaseapp.com",
+  projectId: "air-cnc-5f07c",
+  storageBucket: "air-cnc-5f07c.appspot.com",
+  messagingSenderId: "376686533825",
+  appId: "1:376686533825:web:adab10ab1ed8070eb70840"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app
