cde28fad5087e0bbaa636d8b3756d636904eb1fc
MuntasirAbid <muntasira7@hotmail.com>
2023-02-01 03:14:53 +0100

    checkout page implimented

--- src/Pages/Checkout.js
+++ src/Pages/Checkout.js
@@ -7,37 +7,21 @@ import Payment from '../Components/Checkout/Payment'
 import { AuthContext } from '../contexts/AuthProvider'
 import { saveBooking } from '../api/bookings'
 import toast from 'react-hot-toast'
+import { useLocation } from 'react-router-dom'
 
 const Checkout = () => {
     const { user } = useContext(AuthContext)
-    const homeData = {
-        _id: '60ehjhedhjdj3434',
-        location: 'Dhaka, Bangladesh',
-        title: 'Huge Apartment with 4 bedrooms',
-        image: 'https://i.ibb.co/YPXktqs/Home1.jpg',
-        from: '17/11/2022',
-        to: '21/11/2022',
-        host: {
-            name: 'John Doe',
-            image: 'https://i.ibb.co/6JM5VJF/photo-1633332755192-727a05c4013d.jpg',
-            email: 'johndoe@gmail.com',
-        },
-        price: 98,
-        total_guest: 4,
-        bedrooms: 2,
-        bathrooms: 2,
-        ratings: 4.8,
-        reviews: 64,
-    }
+    const { state: checkoutData } = useLocation()
+
+    const [selectedIndex, setSelectedIndex] = useState(0)
 
     const [bookingData, setBookingData] = useState({
-        homeId: homeData._id,
-        hostEmail: homeData?.host?.email,
+        homeId: checkoutData._id,
+        hostEmail: checkoutData?.host?.email,
         message: '',
-        totalPrice: parseFloat(homeData.price) + 31,
+        totalPrice: parseFloat(checkoutData.price) + 31,
         guestEmail: user?.email,
     })
-    const [selectedIndex, setSelectedIndex] = useState(0)
 
     const handleBooking = () => {
         console.log(bookingData);
@@ -126,13 +110,13 @@ const Checkout = () => {
                     </Tab.List>
                     <Tab.Panels>
                         <Tab.Panel>
-                            <ReviewHouse setSelectedIndex={setSelectedIndex} />
+                            <ReviewHouse setSelectedIndex={setSelectedIndex} homeData={{ ...checkoutData?.homeData, totalNights: checkoutData?.totalNights }} />
                         </Tab.Panel>
                         <Tab.Panel>
 
                             <WhosComing
                                 setSelectedIndex={setSelectedIndex}
-                                host={homeData?.host}
+                                host={checkoutData?.host}
                                 bookingData={bookingData}
                                 setBookingData={setBookingData}
                             />
@@ -146,7 +130,7 @@ const Checkout = () => {
             </div>
 
             {/* Cart */}
-            <CheckoutCart />
+            <CheckoutCart homeData={checkoutData?.homeData} />
         </div>
     )
 }
